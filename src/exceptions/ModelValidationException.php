d5f3343b33c36a1de0b1511d551e02b97c5f9f52
Matrozov <oleg.matrozov@gmail.com>
2025-09-09 08:38:30 +0300

    Compatibility with https://github.com/matrozov/yii2-multiple-field

--- src/exceptions/ModelValidationException.php
+++ src/exceptions/ModelValidationException.php
@@ -5,10 +5,19 @@ namespace matrozov\yii2kit\exceptions;
 use Exception;
 use yii\base\ErrorException;
 use yii\base\Model;
+use yii\helpers\ArrayHelper;
 use yii\helpers\VarDumper;
 
 class ModelValidationException extends ErrorException
 {
+    /**
+     * @param Model $model
+     * @param $message
+     * @param $filename
+     * @param $lineno
+     * @param Exception|null $previous
+     * @throws Exception
+     */
     public function __construct(Model $model, $message = '', $filename = __FILE__, $lineno = __LINE__, Exception $previous = null)
     {
         $delimiter = ' | ';
@@ -22,7 +31,10 @@ class ModelValidationException extends ErrorException
         $result[] = 'Error in ' . get_class($model);
 
         foreach ($model->getFirstErrors() as $field => $error) {
-            $value = $model->$field;
+            // Compatibility with https://github.com/matrozov/yii2-multiple-field
+            $field = str_replace(['[', ']'], ['.', ''], $field);
+
+            $value = ArrayHelper::getValue($model, $field);
 
             $result[] = "err: $error ($field = " . substr(VarDumper::export($value), 0, 255) . ')';
         }
